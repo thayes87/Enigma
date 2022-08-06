59a0f3f6f1254a03563f2c77019a4d9d977eb2d8
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-06 15:46:35 -0600

    create decrypt runner

--- decrypt.rb
+++ decrypt.rb
@@ -1,1 +1,19 @@
-require './enigma'
+require './lib/enigma'
+
+handle = File.open(ARGV[0], "r")
+
+incoming_text = handle.read
+
+handle.close
+
+enigma = Enigma.new
+
+decryption_hash = enigma.decrypt(incoming_text, ARGV[2], ARGV[3])
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(decryption_hash[:decryption])
+
+writer.close
+
+puts "Created '#{ARGV[1]}' with the key #{decryption_hash[:key]} and date #{decryption_hash[:date]}"
