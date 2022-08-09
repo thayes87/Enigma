257a5605570b9c41b9f9ce4e5fd1106ea4c702b0
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-09 16:05:47 -0600

    Rubocop shift_generator.rb fix: round 2

--- lib/shift_generator.rb
+++ lib/shift_generator.rb
@@ -1,5 +1,5 @@
 class ShiftGenerator
-  class << self 
+  class << self
     def build_shift_dictionary(key, date)
       key_shift = build_keys_shift(key)
       offset_shift = build_offset_shift(date)
@@ -7,12 +7,12 @@ class ShiftGenerator
       total_shift << key_shift
       total_shift << offset_shift
       total_shift.reduce do |final_hash, element|
-        final_hash.merge(element) do |_key, old_value, new_value| 
+        final_hash.merge(element) do |_key, old_value, new_value|
           old_value + new_value
         end
       end
     end
-  
+
     def assign_shift(index, shift_dictionary)
       if (index % 4).zero?
         shift_dictionary[:A]
@@ -24,9 +24,9 @@ class ShiftGenerator
         shift_dictionary[:D]
       end
     end
-  
+
     private
-    
+
     def build_keys_shift(key)
       key_shift = Hash.new
       key_shift[:A] = key[0, 2].to_i
@@ -35,7 +35,7 @@ class ShiftGenerator
       key_shift[:D] = key[3, 4].to_i
       key_shift
     end
-  
+
     def build_offset_shift(date)
       square_total = (date.to_i * date.to_i)
       offset_indexes = square_total.digits.reverse.last(4)
@@ -44,7 +44,7 @@ class ShiftGenerator
       offset_shift[:B] = offset_indexes[1]
       offset_shift[:C] = offset_indexes[2]
       offset_shift[:D] = offset_indexes[3]
-      offset_shift  
+      offset_shift
     end
   end
 end
