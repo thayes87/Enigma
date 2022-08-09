f1b4146a60b87a22f30347b454b88e193ce730f3
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-09 16:19:36 -0600

    Rubocop file_reader_spec.rb

--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -2,29 +2,28 @@ require './spec_helper'
 require './file_reader'
 
 RSpec.describe FileReader do
-
   describe '.read_encryption_file' do
     it 'returns encryption_hash' do
-    
+
       expected = {
         encryption: "keder ohulw",
         key: "02715",
         date: "040895"
       }
-      
+
       expect(FileReader.encrypt_file("message.txt", "encrypted.txt")).to eq(expected)
     end
   end
 
   describe '.read_decryption_file' do
     it 'returns decryption_hash' do
-    
+
       expected = {
         decryption: "hello world",
         key: "02715",
         date: "040895"
       }
-      
+
       expect(FileReader.decrypt_file("encrypted.txt", "decrypted.txt", "02715", "040895")).to eq(expected)
     end
   end
