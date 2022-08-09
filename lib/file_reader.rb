221086a47390ec9385bfc22871e8a32b92766c2d
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-09 15:43:33 -0600

    Rubocop file_reader.rb fix

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,6 +1,5 @@
 require './enigma'
 class FileReader
-
   def self.encrypt_file(input_file, output_file)
     incoming_text = read_file(input_file)
     enigma = Enigma.new
@@ -8,32 +7,32 @@ class FileReader
     encryption_hash = enigma.encrypt(incoming_text, "02715", "040895") #=> leaving in for testing purposes
 
     write_file(output_file, encryption_hash)
-    
+
     encryption_hash
-  end 
+  end
 
   def self.decrypt_file(input_file, output_file, key, date)
     incoming_text = read_file(input_file)
     enigma = Enigma.new
 
     decryption_hash = enigma.decrypt(incoming_text, key, date)
-    
+
     write_file(output_file, decryption_hash)
-    
+
     decryption_hash
   end
 
-  private 
+  private
 
   def self.read_file(input_file)
-    handle = File.open(input_file, "r")
+    handle = File.open(input_file, 'r')
     incoming_text = handle.read
-    handle.close 
+    handle.close
     incoming_text
   end
 
   def self.write_file(output_file, encryption_hash)
-    writer = File.open(output_file, "w")
+    writer = File.open(output_file, 'w')
     writer.write(encryption_hash[:encryption])
     writer.close
   end
