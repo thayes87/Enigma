0b592287486370999f0a3712fabe8da64d292522
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-09 15:48:24 -0600

    Rubocop key_generator.rb fix

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,16 +1,14 @@
 class KeyGenerator
-  
+
   def self.generate_key(key)
     if key.nil?
-      number_set = Array("1".."9")
+      number_set = Array('1'..'9')
       Array.new(5) { number_set.sample }.join
-    elsif
-      key.length < 5
+    elsif key.length < 5
       key.rjust(5, "0")
-    elsif
-      key.length > 5
-      key.split("").first(5).join
-    else  
+    elsif key.length > 5
+      key.split('').first(5).join
+    else
       key
     end
   end
