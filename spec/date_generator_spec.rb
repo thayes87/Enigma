d1c5fe2bb605a2bf6d8245cf76517bc0b3bf0acf
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-09 16:12:35 -0600

    Rubocop date_generator_spec.rb fix

--- spec/date_generator_spec.rb
+++ spec/date_generator_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe DateGenerator do
     it 'generates the date of submission if no date is provided' do
 
       expect(described_class.generate_date(nil).size).to eq(6)
-      expect(described_class.generate_date(nil)).to eq(Date.today.strftime ("%m%d%y"))
+      expect(described_class.generate_date(nil)).to eq(Date.today.strftime("%m%d%y"))
     end
 
     it 'can identify and return a valid date' do
