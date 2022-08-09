99dec161f83e96367772ec95f0752688ab1ff4e8
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-09 15:33:36 -0600

    Rubocop date_generator fix

--- lib/date_generator.rb
+++ lib/date_generator.rb
@@ -1,10 +1,9 @@
 require './spec_helper'
 
 class DateGenerator
-
   def self.generate_date(date)
     if date.nil?
-      Date.today.strftime ("%m%d%y")
+      Date.today.strftime('%m%d%y')
     else
       date
     end
