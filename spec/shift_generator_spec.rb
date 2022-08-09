e4e833e48d9b7b9d1222ab4e19022c09e74edb39
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-09 16:25:54 -0600

    Rubocop shift_generator_spec.rb fix

--- spec/shift_generator_spec.rb
+++ spec/shift_generator_spec.rb
@@ -5,27 +5,27 @@ RSpec.describe ShiftGenerator do
   describe '.build_shift_dictionary' do
     it 'returns the total shift based on the keys and offset' do
 
-        expected = {
+      expected = {
           A: 3,
           B: 27,
           C: 73,
-          D: 20,
-        }
+          D: 20
+      }
 
       expect(described_class.build_shift_dictionary("02715", "040895")).to eq(expected)
     end
   end
-  
+
   describe '#assign_shift' do
     it 'identifies the shift based on the index postion' do
-      
+
       shift_dictionary = {
         A: 3,
         B: 27,
         C: 73,
-        D: 20,
+        D: 20
       }
-           
+    
       expect(described_class.assign_shift(0, shift_dictionary)).to eq(3)
       expect(described_class.assign_shift(1, shift_dictionary)).to eq(27)
       expect(described_class.assign_shift(2, shift_dictionary)).to eq(73)
