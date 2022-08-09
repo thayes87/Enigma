b0d12232b6f8b2e5746faac7b4d2b32671d125dd
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-09 14:20:49 -0600

    Constant variables for unchanging data

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,21 +4,21 @@ require './key_generator'
 require './date_generator'
 
 class Enigma
+  ALPHABET_SET = ("a".."z").to_a << " "
  
   def encrypt(message, key = nil, date = nil)
     key = KeyGenerator.generate_key(key)
     date = DateGenerator.generate_date(date)
     modified_message = message.downcase.split("")
     encrypt_message = ""
-    alphabet_set = ("a".."z").to_a << " " 
     shift_dictionary = ShiftGenerator.build_shift_dictionary(key, date)
     
     modified_message.each_with_index do |character, index|
-      if alphabet_set.include?(character)
-        alphabet_position = alphabet_set.index(character)
+      if ALPHABET_SET.include?(character)
+        alphabet_position = ALPHABET_SET.index(character)
         shift = ShiftGenerator.assign_shift(index, shift_dictionary)
-        new_index = (alphabet_position + shift) % alphabet_set.count
-        encrypt_message << alphabet_set[new_index]
+        new_index = (alphabet_position + shift) % ALPHABET_SET.count
+        encrypt_message << ALPHABET_SET[new_index]
       else
         encrypt_message << character 
       end
@@ -33,15 +33,14 @@ class Enigma
   def decrypt(message, key, date)
     modified_message = message.downcase.split("")
     decrypt_message = ""
-    alphabet_set = ("a".."z").to_a << " "
     shift_dictionary = ShiftGenerator.build_shift_dictionary(key, date)
     
     modified_message.each_with_index do |character, index|
-      if alphabet_set.include?(character)
-        alphabet_position = alphabet_set.index(character)
+      if ALPHABET_SET.include?(character)
+        alphabet_position = ALPHABET_SET.index(character)
         shift = ShiftGenerator.assign_shift(index, shift_dictionary)
-        new_index = (alphabet_position - shift) % alphabet_set.count
-        decrypt_message << alphabet_set[new_index]
+        new_index = (alphabet_position - shift) % ALPHABET_SET.count
+        decrypt_message << ALPHABET_SET[new_index]
       else
         decrypt_message << character
       end
